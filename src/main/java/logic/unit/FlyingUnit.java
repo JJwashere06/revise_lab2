ea77950501f06857ab03b0dc073a10c5c1984a96
Janjao <janjao181930@gmail.com>
2026-01-30 16:10:06 +0700

    flyingUnitUpdate

--- src/main/java/logic/unit/FlyingUnit.java
+++ src/main/java/logic/unit/FlyingUnit.java
@@ -6,4 +6,24 @@ public class FlyingUnit extends BaseUnit{
         this.setHp(2);
         this.isFlying=true;
     }
+    public boolean move(int direction){
+        if(direction>=0 && direction<=3){
+            if(direction==0){
+                setRow(getRow()+2);
+            }
+            else if(direction==1){
+                setColumn(getColumn()+2);
+            }
+            else if(direction==2){
+                setRow(getRow()-2);
+            }
+            else{
+                setColumn(getColumn()-2);
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
